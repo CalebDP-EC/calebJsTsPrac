a05ff4a23d8e5ae95a68b08f506470a99b079e2e
Caleb Du Preez <caleb.dupreez@ec.co.za>
2026-01-09 12:22:43 +0000

    Started practicing code so added Exercise 1

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 const window = "This is a test string for the window variable.";
 
 console.log(window);
+// Note: In a browser environment, 'window' is a global object.
+// In this Node.js environment, we are using 'window' as a regular variable.
+
+//Exercise 1
+console.log(26);
